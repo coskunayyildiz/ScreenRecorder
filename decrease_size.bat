71563671f0be9b1a6e0ba7fd3ef3c6f9d7f01700
coskunayyildiz <csknyyldz@yahoo.com>
2023-10-09 23:40:22 +0300

    size azaltma calismalari

--- decrease_size.bat
+++ decrease_size.bat
@@ -1,17 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "inputFolder=."
-set "ffmpegPath=ffmpeg.exe"
+:: Set the input and output folder paths
+set "input_folder=C:\Users\cskny\source\repos\ScreenRecorder\bin\Debug\net6.0-windows\Videos\09102023"
+set "output_folder=%input_folder%\output"
 
-for %%f in ("%inputFolder%\*.avi") do (
-    set "inputFile=%%~nf%%~xf"
-    
-    echo Processing !inputFile!...
-    
-    "%ffmpegPath%" -i "%%f" -vf "scale=1280:720" -b:v 1500k -c:v libx264 -c:a aac -strict experimental -b:a 128k -y "!inputFile!"
-    
-    echo !inputFile! processed and saved in the same folder.
+:: Create the output folder if it doesn't exist
+if not exist "%output_folder%" (
+    mkdir "%output_folder%"
 )
 
-endlocal
+:: Loop through .avi files in the input folder
+for %%f in ("%input_folder%\*.avi") do (
+    echo Processing "%%~nxf"
+    
+    :: Use FFmpeg to compress the video and save it in the output folder
+    ffmpeg -i "%%f" -c:v libx264 -crf 23 -c:a aac -strict experimental -b:a 128k "%output_folder%\%%~nf_compressed.mp4"
+    
+    echo "%%~nxf" compressed and saved as "%%~nf_compressed.mp4"
+)
+
+echo All videos processed.
+pause
