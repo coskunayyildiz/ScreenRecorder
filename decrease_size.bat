4da2802a0c8877fd302ddf57f064ab08b768b467
coskunayyildiz <csknyyldz@yahoo.com>
2023-10-11 21:15:44 +0300

    video size decrease .bat file düzeltildi.

--- decrease_size.bat
+++ decrease_size.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Set the input and output folder paths
-set "input_folder=C:\Users\cskny\source\repos\ScreenRecorder\bin\Debug\net6.0-windows\Videos\09102023"
+set "input_folder=."
 set "output_folder=%input_folder%\output"
 
 :: Create the output folder if it doesn't exist
@@ -15,9 +15,11 @@ for %%f in ("%input_folder%\*.avi") do (
     echo Processing "%%~nxf"
     
     :: Use FFmpeg to compress the video and save it in the output folder
-    ffmpeg -i "%%f" -c:v libx264 -crf 23 -c:a aac -strict experimental -b:a 128k "%output_folder%\%%~nf_compressed.mp4"
+    ffmpeg -i "%%f" -c:v libx264 -crf 18 -c:a aac -strict experimental -b:a 128k "%output_folder%\%%~nf_compressed.avi"
     
-    echo "%%~nxf" compressed and saved as "%%~nf_compressed.mp4"
+    echo "%%~nxf" compressed and saved as "%%~nf.avi"
+
+    del "%%~nxf"
 )
 
 echo All videos processed.
